acb61e3d1e68426ddd7e8ce70eefa00c63e1afdd
Aldi <aldi123@gmail.com>
2025-05-15 23:44:31 +0700

    menambah master data pegawai

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -9,4 +9,46 @@ class Pegawai extends Model
 {
     /** @use HasFactory<\Database\Factories\PegawaiFactory> */
     use HasFactory;
+
+    protected $table = 'pegawai'; // atau 'pegawai', sesuaikan dengan nama tabelmu
+    protected $primaryKey = 'id_pegawai';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'id_pegawai',
+        'nama_pegawai',
+        'alamat',
+        'no_hp',
+        'shift_kerja'
+    ];
+
+    public static function generateNewId()
+{
+    $lastId = self::orderBy('id_pegawai', 'desc')->first()?->id_pegawai;
+
+    if ($lastId && preg_match('/PG(\d+)/', $lastId, $matches)) {
+        $number = (int) $matches[1] + 1;
+    } else {
+        $number = 1;
+    }
+
+    return 'PG' . str_pad($number, 3, '0', STR_PAD_LEFT);
+}
+
+
+protected static function boot()
+{
+    parent::boot();
+
+    static::creating(function ($model) {
+        if (empty($model->id_pegawai)) {
+            $model->id_pegawai = self::generateNewId();
+        }
+    });
+
+    
+}
+
+
 }
